cad1213be7c2d76462b0a657df4f127c4e35fc80
Diego Bernardes Babo <diegobbabo@gmail.com>
2023-08-28 16:55:51 -0300

    feat: desafio 10

---
+++ src/desafios/desafio10.java
@@ -0,0 +1,16 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class desafio10 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3, 11);
+
+        numeros.stream()
+                .distinct()
+                .filter(n -> n % 3 == 0 || n % 5 == 0)
+                .forEach(System.out::println);
+    }
+}
